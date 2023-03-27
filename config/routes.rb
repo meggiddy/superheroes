322a7d8e0e86c939b0767a6c0a5246e1fb104905
Maggie Muriithi <muriithinyawira13@gmail.com>
2023-03-27 09:09:01 +0300

    controllers generated

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  resources :heroes, only: [:index, :show]
+  resources :powers, only: [:index, :show, :update] do
+    post 'hero_powers', on: :collection
+  end
 end
